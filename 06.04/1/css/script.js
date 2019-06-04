a93ca448f61d0ada5d07257b3158d23ddea3fb07
Pavel <p.savlevic@gmail.com>
2019-06-04 16:26:06 +0300

    suvedus skaicius i inputus, rodo suma zemiau

---
+++ 06.04/1/css/script.js
@@ -0,0 +1,8 @@
+'use strict';
+function fn() {
+var pirmas = Number(document.getElementById("pirmas").value);
+var antras = Number(document.getElementById("antras").value);
+var show = document.getElementById("ats");
+var sum = pirmas + antras;
+ats.innerHTML = sum;
+}
