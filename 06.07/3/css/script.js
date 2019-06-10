f2405a92b2eb398ab451fe58720bd69de9dd0f58
Pavel <p.savlevic@gmail.com>
2019-06-10 09:38:18 +0300

    Homework 06.10 (arrejaus printinimas html'e su createTextNode)

--- 06.07/3/css/script.js
+++ 06.07/3/css/script.js
@@ -1,11 +1,1 @@
 'use strict';
-document.querySelector("input[type=submit]")
-    .addEventListener("click", fn);
-
-function fn() {
-var inpt = document.getElementById("inptekstas").value;
-var tekstas = document.createTextNode(inpt);
-document.getElementById("app").prepend(tekstas);
-}
-
-
